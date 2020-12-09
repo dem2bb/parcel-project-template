ae061107168887b7648cada226669b964a4bd0c5
Maxim Kazachuk <dem2bb1@gmail.com>
2020-12-09 12:04:46 +0200

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -163,7 +163,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64910" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55570" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
