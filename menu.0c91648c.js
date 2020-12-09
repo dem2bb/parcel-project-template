4d6e623225cca8188fd5a47cb4b30000e31bc305
Maxim Kazachuk <dem2bb1@gmail.com>
2020-12-09 21:01:51 +0200

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -121,7 +121,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 (function () {
   var menuBtnRef = document.querySelector("[data-menu-button]");
   var mobileMenuRef = document.querySelector("[data-menu]");
-  var closeMenuBtn = document.querySelector("[close-menu-button]");
+  var closeMenuBtn = document.querySelector("[data-close-menu-button]");
   menuBtnRef.addEventListener("click", function () {
     var expanded = // menuBtnRef.getAttribute("aria-expanded") === "true" || false;
     menuBtnRef.classList.toggle("is-open"); // menuBtnRef.setAttribute("aria-expanded", !expanded);
@@ -163,7 +163,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55570" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61664" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
